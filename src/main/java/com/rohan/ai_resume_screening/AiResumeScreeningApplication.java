ef78cd66c93bf39f95c0d079129d63bb6caf813c
Rohan Patil <rohanpatil1216@gmail.com>
2026-02-24 10:13:18 +0530

    Initial commit

---
+++ src/main/java/com/rohan/ai_resume_screening/AiResumeScreeningApplication.java
@@ -0,0 +1,13 @@
+package com.rohan.ai_resume_screening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiResumeScreeningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiResumeScreeningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rohan/ai_resume_screening/AiResumeScreeningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohan.ai_resume_screening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiResumeScreeningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
